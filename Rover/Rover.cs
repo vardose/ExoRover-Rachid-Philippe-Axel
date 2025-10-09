ecd577d3a7fdfd391617739267add4ad4811d13d
philippe-p <134625964+Philippe-SP@users.noreply.github.com>
2025-10-09 14:17:43 +0200

    Création class Rover

--- Rover/Rover.cs
+++ Rover/Rover.cs
@@ -1,7 +1,53 @@
-using ExoRover
+using ExoRover;
+
 namespace Rover;
 
 public class Rover
 {
-    
+    #region Fields
+
+    public enum CardinalPoints
+    {
+        North,
+        South,
+        East,
+        West
+    }
+
+    private Position position;
+    private bool isObstacle;
+    private CardinalPoints orientation;
+
+    #endregion
+
+    #region Properties
+
+    public Position Position
+    {
+        get => position;
+        set => position = value;
+    }
+
+    public bool IsObstacle
+    {
+        get => isObstacle;
+        set => isObstacle = value;
+    }
+
+    public CardinalPoints Orientation
+    {
+        get => orientation;
+        set => orientation = value;
+    }
+
+    #endregion
+
+    #region Constructors
+
+    public Rover(Position position, bool isObstacle, CardinalPoints orientation)
+    {
+
+    }
+
+    #endregion
 }
