355e3b6f0a95ae5e6323a718abc8e69447fcf8b8
Rezig rachid <rachid.rezig69@gmail.com>
2025-10-10 10:16:11 +0200

    refacto

--- ExoRover/Map.cs
+++ ExoRover/Map.cs
@@ -1,46 +1,68 @@
-namespace ExoRover;
-
-public class Map
+namespace ExoRover
 {
-    #region Fields
-
-    private string mapName;
-    bool [][] ObstacleMap = new bool [10][] ;
-
-    #endregion
-
-    #region Properties
-
-    public string MapName
+    public class Map
     {
-        get => mapName;
-        set => mapName = value;
+        #region Fields
+
+        private string mapName;
+        private bool[][] ObstacleMap = new bool[10][]; // 10 colonnes
+
+        #endregion
+
+        #region Properties
+
+        public string MapName
+        {
+            get => mapName;
+            set => mapName = value;
+        }
+
+        #endregion
+
+        #region Constructors
+
+        public Map()
+        {
+            InitializeObstacleMap();
+        }
+
+        public Map(string mapName)
+        {
+            this.mapName = mapName;
+            InitializeObstacleMap();
+        }
+
+        private void InitializeObstacleMap()
+        {
+            for (int i = 0; i < ObstacleMap.Length; i++)
+            {
+                ObstacleMap[i] = new bool[10];  // chaque ligne = 10 colonnes
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void AddObstacleMap(Obstacle obstacle)
+        {
+            int x = obstacle.Positions.Longitude;
+            int y = obstacle.Positions.Latitude;
+
+            if (x < 0 || x >= ObstacleMap.Length || y < 0 || y >= ObstacleMap[x].Length)
+                throw new ArgumentOutOfRangeException("Les coordonnées de l'obstacle sont en dehors de la carte.");
+
+            ObstacleMap[x][y] = true;
+        }
+
+        public bool HasObstacle(int x, int y)
+        {
+            if (x < 0 || x >= ObstacleMap.Length || y < 0 || y >= ObstacleMap[x].Length)
+                return false;
+
+            return ObstacleMap[x][y];
+        }
+
+        #endregion
     }
-
-   
-    
-    
-    
-    #endregion
-
-    #region Constructors
-
-    public Map()
-    {
-    }
-
-    public Map(string mapName)
-    {
-    }
-
-
-    public void AddObstacleMap(Obstacle obstacle)
-    {
-        ObstacleMap[obstacle.Positions.Longitude][obstacle.Positions.Latitude] = true;
-
-    }
-    
-    
-
-    #endregion
 }
